a57cb0fb467b24cb173c47d9a9e71647c13d4408
alexcoman1 <octavianalexandru.coman@zebra.com>
2023-03-31 19:48:16 +0200

    update

--- project.py
+++ project.py
@@ -1,9 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '006ec4efeb2aafc82f298cf16bff31ac'
+
+posts = [
+    {
+        'author': 'Alex Coman',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
 @app.route('/')
+@app.route("/home")
 def hello():
-    return 'Hello, World!'
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html',  title='About')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
 
 
 
